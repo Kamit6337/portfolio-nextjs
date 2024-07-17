01170503c88e803b00e5fac713dcaed1c09b8299
Kamit6337 <kamit6337@gmail.com>
2024-07-17 10:45:36 +0530

    add direct GA4 Measurement ID\

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize(process.env.GA4_MEASUREMENT_ID as string); // Replace with your Measurement ID
+  ReactGA.initialize("G-VZZPY370NH"); // Replace with your Measurement ID
 };
 
 export const logPageView = (url: string) => {
